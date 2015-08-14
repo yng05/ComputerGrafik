7cf010d53393e1d20c55ca9bd9915af9490d6b8d
wobakj <jakob.wagner@uni-weimar.de>
2015-08-14 21:22:44 +0200

    remove unnecessary include from shader_loader

--- framework/include/shader_loader.hpp
+++ framework/include/shader_loader.hpp
@@ -1,13 +1,12 @@
-#ifndef SHADER_HPP
-#define SHADER_HPP
+#ifndef SHADER_LOADER_HPP
+#define SHADER_LOADER_HPP
 
 #include <glbinding/gl/gl.h>
-#include <glbinding/Binding.h>
 
 using namespace gl;
 
 namespace shader_loader {
-  GLuint shader(std::string const& file_name, GLenum shaderType)/*Takes a filename and a Shadertype and returns an id of the compiled shader; shaderType could be: GL_VERTEX_SHADER, GL_FRAGMENT_SHADER, GL_GEOMETRIE_SHADER*/;
+  GLuint shader(std::string const& file_name, GLenum shader_type);
   
   GLuint program(std::string const& vertex_name, std::string const& fragment_name);
 };
---
+++ framework/include/texture_loader.hpp
@@ -0,0 +1,12 @@
+#ifndef TEXTURE_LOADER_HPP
+#define TEXTURE_LOADER_HPP
+
+#include <glbinding/gl/gl.h>
+
+using namespace gl;
+
+namespace texture_loader {
+
+};
+
+#endif
