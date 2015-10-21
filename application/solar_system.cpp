6596163219670bc8865954fc131940c5dd94aa0e
wobakj <jakob.wagner@uni-weimar.de>
2015-10-21 13:50:31 +0200

    add forward declarations and change function order in main program

--- application/solar_system.cpp
+++ application/solar_system.cpp
@@ -1,4 +1,5 @@
 
+///////////////////////////////// includes ///////////////////////////////////
 #include <glbinding/gl/gl.h>
 // load glbinding extensions
 #include <glbinding/Binding.h>
@@ -25,19 +26,22 @@
 // use gl definitions from glbinding 
 using namespace gl;
 
+/////////////////////////// variable definitions //////////////////////////////
 // vertical field of view of camera
 const float camera_fov = glm::radians(60.0f);
 // initial window dimensions
 const unsigned window_width = 640;
 const unsigned window_height = 480;
-
 // the rendering window
 GLFWwindow* window;
+
 // variables for fps computation
 double last_second_time = 0;
 unsigned frames_per_second = 0;
+
 // the main shader program
 GLuint simple_program = 0;
+
 // cpu representation of model
 model planet_model{};
 // holds gpu representation of model
@@ -47,174 +51,32 @@ struct model_object {
   GLuint element_BO = 0;
 };
 model_object planet_object;
+
 // camera matrices
 glm::mat4 camera_view = glm::translate(glm::mat4{}, glm::vec3{0.0f, 0.0f, 2.0f});
-glm::mat4 camera_projection{};
+glm::mat4 camera_projection{1.0f};
+
 // uniform locations
 GLint location_normal_matrix = -1;
 GLint location_model_matrix = -1;
 GLint location_view_matrix = -1;
 GLint location_projection_matrix = -1;
+
 // path to the resource folders
 std::string resource_path{};
 
-void quit(int status) {
-  // free opengl resources
-  glDeleteProgram(simple_program);
-  glDeleteBuffers(1, &planet_object.vertex_BO);
-  glDeleteVertexArrays(1, &planet_object.element_BO);
-  glDeleteVertexArrays(1, &planet_object.vertex_AO);
+/////////////////////////// forward declarations //////////////////////////////
+void quit(int status);
+void update_view(GLFWwindow* window, int width, int height);
+void update_camera();
+void update_uniform_locations();
+void update_shader_programs();
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+void initialize_geometry();
+void show_fps();
+void render();
 
-  // free glfw resources
-  glfwDestroyWindow(window);
-  glfwTerminate();
-
-  std::exit(status);
-}
-
-// update viewport and field of view
-void update_view(GLFWwindow* window, int width, int height) {
-  // resize framebuffer
-  glViewport(0, 0, width, height);
-
-  float aspect = float(width) / height;
-  float fov_y = camera_fov;
-  // if width is smaller, extend vertical fov 
-  if(width < height) {
-    fov_y = 2.0f * glm::atan(glm::tan(camera_fov * 0.5f) * (1.0f / aspect));
-  }
-  // projection is hor+ 
-  camera_projection = glm::perspective(fov_y, aspect, 0.1f, 10.0f);
-  // upload matrix to gpu
-  glUniformMatrix4fv(location_projection_matrix, 1, GL_FALSE, glm::value_ptr(camera_projection));
-}
-
-// update camera transformation
-void update_camera() {
-  // vertices are transformed in camera space, so camera transform must be inverted
-  glm::mat4 inv_camera_view = glm::inverse(camera_view);
-  // upload matrix to gpu
-  glUniformMatrix4fv(location_view_matrix, 1, GL_FALSE, glm::value_ptr(inv_camera_view));
-}
-
-// update shader uniform locations
-void update_uniform_locations() {
-  location_normal_matrix = glGetUniformLocation(simple_program, "NormalMatrix");
-  location_model_matrix = glGetUniformLocation(simple_program, "ModelMatrix");
-  location_view_matrix = glGetUniformLocation(simple_program, "ViewMatrix");
-  location_projection_matrix = glGetUniformLocation(simple_program, "ProjectionMatrix");
-}
-
-// load shaders and update uniform locations
-void update_shader_programs() {
-  try {
-    // throws exception when compiling was unsuccessfull
-    GLuint new_program = shader_loader::program(resource_path + "shaders/simple.vert", resource_path + "shaders/simple.frag");
-    // free old shader
-    glDeleteProgram(simple_program);
-    // save new shader
-    simple_program = new_program;
-    // bind shader
-    glUseProgram(simple_program);
-    // after shader is recompiled uniform locations may change
-    update_uniform_locations();
-
-    // upload view uniforms to new shader
-    int width, height;
-    glfwGetFramebufferSize(window, &width, &height);
-    update_view(window, width, height);
-    update_camera();
-  }
-  catch(std::exception& e) {
-    // dont crash, allow another try
-  }
-}
-
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-  if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-    glfwSetWindowShouldClose(window, 1);
-  }
-  else if(key == GLFW_KEY_R && action == GLFW_PRESS) {
-    update_shader_programs();
-  }
-  else if(key == GLFW_KEY_W && action == GLFW_PRESS) {
-    camera_view = glm::translate(camera_view, glm::vec3{0.0f, 0.0f, -0.1f});
-    update_camera();
-  }
-  else if(key == GLFW_KEY_S && action == GLFW_PRESS) {
-    camera_view = glm::translate(camera_view, glm::vec3{0.0f, 0.0f, 0.1f});
-    update_camera();
-  }
-}
-
-void initialize_shaders() {
-  // load shader program
-  simple_program = shader_loader::program(resource_path + "shaders/simple.vert", resource_path + "shaders/simple.frag");
-  // reload uniform locations
-  update_shader_programs();
-}
-
-// load model
-void initialize_geometry() {
-  planet_model = model_loader::obj(resource_path + "models/sphere.obj", model::NORMAL);
-
-  // generate vertex array object
-  glGenVertexArrays(1, &planet_object.vertex_AO);
-  // bind the array for attaching buffers
-  glBindVertexArray(planet_object.vertex_AO);
-
-  // generate generic buffer
-  glGenBuffers(1, &planet_object.vertex_BO);
-  // bind this as an vertex array buffer containing all attributes
-  glBindBuffer(GL_ARRAY_BUFFER, planet_object.vertex_BO);
-  // configure currently bound array buffer
-  glBufferData(GL_ARRAY_BUFFER, sizeof(float) * planet_model.data.size(), planet_model.data.data(), GL_STATIC_DRAW);
-
-  // activate first attribute on gpu
-  glEnableVertexAttribArray(0);
-  // first attribute is 3 floats with no offset & stride
-  glVertexAttribPointer(0, model::POSITION.components, model::POSITION.type, GL_FALSE, planet_model.vertex_bytes, planet_model.offsets[model::POSITION]);
-  // activate second attribute on gpu
-  glEnableVertexAttribArray(1);
-  // second attribute is 3 floats with no offset & stride
-  glVertexAttribPointer(1, model::NORMAL.components, model::NORMAL.type, GL_FALSE, planet_model.vertex_bytes, planet_model.offsets[model::NORMAL]);
-
-   // generate generic buffer
-  glGenBuffers(1, &planet_object.element_BO);
-  // bind this as an vertex array buffer containing all attributes
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, planet_object.element_BO);
-  // configure currently bound array buffer
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, model::INDEX.size * planet_model.indices.size(), planet_model.indices.data(), GL_STATIC_DRAW);
-}
-
-// calculate fps and show in window title
-void show_fps() {
-  ++frames_per_second;
-  double current_time = glfwGetTime();
-  if(current_time - last_second_time >= 1.0) {
-    std::string title{"OpenGL Framework - "};
-    title += std::to_string(frames_per_second) + " fps";
-
-    glfwSetWindowTitle(window, title.c_str());
-    frames_per_second = 0;
-    last_second_time = current_time;
-  }
-}
-
-// render model
-void render() {
-  glm::mat4 model_matrix = glm::rotate(glm::mat4{}, float(glfwGetTime()), glm::vec3{0.0f, 1.0f, 0.0f});
-  glUniformMatrix4fv(location_model_matrix, 1, GL_FALSE, glm::value_ptr(model_matrix));
-  // extra matrix for normal transformation to keep them orthogonal to surface
-  glm::mat4 normal_matrix = glm::inverseTranspose(camera_view * model_matrix);
-  glUniformMatrix4fv(location_normal_matrix, 1, GL_FALSE, glm::value_ptr(normal_matrix));
-
-  glBindVertexArray(planet_object.vertex_AO);
-  utils::validate_program(simple_program);
-  // draw bound vertex array as triangles using bound shader
-  glDrawElements(GL_TRIANGLES, GLsizei(planet_model.indices.size()), model::INDEX.type, NULL);
-}
-
+/////////////////////////////// main function /////////////////////////////////
 int main(int argc, char* argv[]) {
 
   glfwSetErrorCallback(utils::glsl_error);
@@ -223,7 +85,7 @@ int main(int argc, char* argv[]) {
     std::exit(EXIT_FAILURE);  
   }
 
-//on MacOS, set OGL version explicitlz  
+//on MacOS, set OGL version explicitly 
 #ifdef __APPLE__
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
@@ -267,7 +129,7 @@ int main(int argc, char* argv[]) {
   }
 
   // do before framebuffer_resize call as it requires the projection uniform location
-  initialize_shaders();
+  update_shader_programs();
 
   // initialize projection and view matrices
   int width, height;
@@ -284,16 +146,173 @@ int main(int argc, char* argv[]) {
   
   // rendering loop
   while(!glfwWindowShouldClose(window)) {
+    // query input
     glfwPollEvents();
-    
+    // clear buffer
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
+    // draw geometry
     render();
-
+    // swap draw buffer to front
     glfwSwapBuffers(window);
-
+    // display fps
     show_fps();
   }
 
   quit(EXIT_SUCCESS);
+}
+
+///////////////////////// initialisation functions ////////////////////////////
+// load models
+void initialize_geometry() {
+  planet_model = model_loader::obj(resource_path + "models/sphere.obj", model::NORMAL);
+
+  // generate vertex array object
+  glGenVertexArrays(1, &planet_object.vertex_AO);
+  // bind the array for attaching buffers
+  glBindVertexArray(planet_object.vertex_AO);
+
+  // generate generic buffer
+  glGenBuffers(1, &planet_object.vertex_BO);
+  // bind this as an vertex array buffer containing all attributes
+  glBindBuffer(GL_ARRAY_BUFFER, planet_object.vertex_BO);
+  // configure currently bound array buffer
+  glBufferData(GL_ARRAY_BUFFER, sizeof(float) * planet_model.data.size(), planet_model.data.data(), GL_STATIC_DRAW);
+
+  // activate first attribute on gpu
+  glEnableVertexAttribArray(0);
+  // first attribute is 3 floats with no offset & stride
+  glVertexAttribPointer(0, model::POSITION.components, model::POSITION.type, GL_FALSE, planet_model.vertex_bytes, planet_model.offsets[model::POSITION]);
+  // activate second attribute on gpu
+  glEnableVertexAttribArray(1);
+  // second attribute is 3 floats with no offset & stride
+  glVertexAttribPointer(1, model::NORMAL.components, model::NORMAL.type, GL_FALSE, planet_model.vertex_bytes, planet_model.offsets[model::NORMAL]);
+
+   // generate generic buffer
+  glGenBuffers(1, &planet_object.element_BO);
+  // bind this as an vertex array buffer containing all attributes
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, planet_object.element_BO);
+  // configure currently bound array buffer
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, model::INDEX.size * planet_model.indices.size(), planet_model.indices.data(), GL_STATIC_DRAW);
+}
+
+///////////////////////////// render functions ////////////////////////////////
+// render model
+void render() {
+  glm::mat4 model_matrix = glm::rotate(glm::mat4{}, float(glfwGetTime()), glm::vec3{0.0f, 1.0f, 0.0f});
+  glUniformMatrix4fv(location_model_matrix, 1, GL_FALSE, glm::value_ptr(model_matrix));
+  // extra matrix for normal transformation to keep them orthogonal to surface
+  glm::mat4 normal_matrix = glm::inverseTranspose(camera_view * model_matrix);
+  glUniformMatrix4fv(location_normal_matrix, 1, GL_FALSE, glm::value_ptr(normal_matrix));
+
+  glBindVertexArray(planet_object.vertex_AO);
+  utils::validate_program(simple_program);
+  // draw bound vertex array as triangles using bound shader
+  glDrawElements(GL_TRIANGLES, GLsizei(planet_model.indices.size()), model::INDEX.type, NULL);
+}
+
+///////////////////////////// update functions ////////////////////////////////
+// update viewport and field of view
+void update_view(GLFWwindow* window, int width, int height) {
+  // resize framebuffer
+  glViewport(0, 0, width, height);
+
+  float aspect = float(width) / height;
+  float fov_y = camera_fov;
+  // if width is smaller, extend vertical fov 
+  if(width < height) {
+    fov_y = 2.0f * glm::atan(glm::tan(camera_fov * 0.5f) * (1.0f / aspect));
+  }
+  // projection is hor+ 
+  camera_projection = glm::perspective(fov_y, aspect, 0.1f, 10.0f);
+  // upload matrix to gpu
+  glUniformMatrix4fv(location_projection_matrix, 1, GL_FALSE, glm::value_ptr(camera_projection));
+}
+
+// update camera transformation
+void update_camera() {
+  // vertices are transformed in camera space, so camera transform must be inverted
+  glm::mat4 inv_camera_view = glm::inverse(camera_view);
+  // upload matrix to gpu
+  glUniformMatrix4fv(location_view_matrix, 1, GL_FALSE, glm::value_ptr(inv_camera_view));
+}
+
+// load shaders and update uniform locations
+void update_shader_programs() {
+  try {
+    // throws exception when compiling was unsuccessfull
+    GLuint new_program = shader_loader::program(resource_path + "shaders/simple.vert",
+                                                resource_path + "shaders/simple.frag");
+    // free old shader
+    glDeleteProgram(simple_program);
+    // save new shader
+    simple_program = new_program;
+    // bind shader
+    glUseProgram(simple_program);
+    // after shader is recompiled uniform locations may change
+    update_uniform_locations();
+
+    // upload view uniforms to new shader
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    update_view(window, width, height);
+    update_camera();
+  }
+  catch(std::exception& e) {
+    // dont crash, allow another try
+  }
+}
+
+// update shader uniform locations
+void update_uniform_locations() {
+  location_normal_matrix = glGetUniformLocation(simple_program, "NormalMatrix");
+  location_model_matrix = glGetUniformLocation(simple_program, "ModelMatrix");
+  location_view_matrix = glGetUniformLocation(simple_program, "ViewMatrix");
+  location_projection_matrix = glGetUniformLocation(simple_program, "ProjectionMatrix");
+}
+
+///////////////////////////// misc functions ////////////////////////////////
+// handle key input
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+  if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, 1);
+  }
+  else if(key == GLFW_KEY_R && action == GLFW_PRESS) {
+    update_shader_programs();
+  }
+  else if(key == GLFW_KEY_W && action == GLFW_PRESS) {
+    camera_view = glm::translate(camera_view, glm::vec3{0.0f, 0.0f, -0.1f});
+    update_camera();
+  }
+  else if(key == GLFW_KEY_S && action == GLFW_PRESS) {
+    camera_view = glm::translate(camera_view, glm::vec3{0.0f, 0.0f, 0.1f});
+    update_camera();
+  }
+}
+
+// calculate fps and show in window title
+void show_fps() {
+  ++frames_per_second;
+  double current_time = glfwGetTime();
+  if(current_time - last_second_time >= 1.0) {
+    std::string title{"OpenGL Framework - "};
+    title += std::to_string(frames_per_second) + " fps";
+
+    glfwSetWindowTitle(window, title.c_str());
+    frames_per_second = 0;
+    last_second_time = current_time;
+  }
+}
+
+void quit(int status) {
+  // free opengl resources
+  glDeleteProgram(simple_program);
+  glDeleteBuffers(1, &planet_object.vertex_BO);
+  glDeleteVertexArrays(1, &planet_object.element_BO);
+  glDeleteVertexArrays(1, &planet_object.vertex_AO);
+
+  // free glfw resources
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
+  std::exit(status);
 }
