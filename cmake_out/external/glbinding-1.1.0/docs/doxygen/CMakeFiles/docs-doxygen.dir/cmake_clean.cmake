620905d7ef06a63166936d05f5bf3905659d12a1
jonas-koehler <jonas.koehler.ks@googlemail.com>
2015-10-28 16:15:40 +0100

    ignore cmake_out

--- cmake_out/external/glbinding-1.1.0/docs/doxygen/CMakeFiles/docs-doxygen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/docs-doxygen"
-  "html/index.hhp"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/docs-doxygen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- cmake_out/external/glbinding-1.1.0/packages/CMakeFiles/pack-glbinding.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/pack-glbinding"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/pack-glbinding.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
