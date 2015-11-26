c43635d30ec9505aa2b98809e3f8c292b6aab0ae
wobakj <jakob.wagner@uni-weimar.de>
2015-11-26 11:48:03 +0100

    add model_loader::generate_tangents function for implementation in exercise

--- framework/source/model_loader.cpp
+++ framework/source/model_loader.cpp
@@ -9,6 +9,10 @@
 
 namespace model_loader {
 
+void generate_normals(tinyobj::mesh_t& model);
+
+std::vector<glm::vec3> generate_tangents(tinyobj::mesh_t const& model);
+
 model obj(std::string const& name, model::attrib_flag_t import_attribs){
   std::vector<tinyobj::shape_t> shapes;
   std::vector<tinyobj::material_t> materials;
@@ -108,6 +112,43 @@ void generate_normals(tinyobj::mesh_t& model) {
     model.normals[i * 3 + 1] = normal[1];
     model.normals[i * 3 + 2] = normal[2];
   }
+}
+
+std::vector<glm::vec3> generate_tangents(tinyobj::mesh_t const& model) {
+  // containers for vetex attributes
+  std::vector<glm::vec3> positions(model.positions.size() / 3);
+  std::vector<glm::vec3> normals(model.positions.size() / 3);
+  std::vector<glm::vec2> texcoords(model.positions.size() / 3);
+  std::vector<glm::vec3> tangents(model.positions.size() / 3, glm::vec3{0.0f});
+
+  // get vertex positions and texture coordinates from mesh_t
+  for (unsigned i = 0; i < model.positions.size(); i+=3) {
+    positions[i / 3] = glm::vec3{model.positions[i],
+                                 model.positions[i + 1],
+                                 model.positions[i + 2]};
+    normals[i / 3] = glm::vec3{model.normals[i],
+                                 model.normals[i + 1],
+                                 model.normals[i + 2]};
+  }
+  for (unsigned i = 0; i < model.texcoords.size(); i+=2) {
+    texcoords[i / 2] = glm::vec2{model.texcoords[i], model.texcoords[i + 1]};
+  }
+
+  // calculate tangent for triangles
+  for (unsigned i = 0; i < model.indices.size() / 3; i++) {
+    // indices of vertices of this triangle, access any attribute of first vert with "attribute[indices[0]]"
+    unsigned indices[3] = {model.indices[i * 3],
+                           model.indices[i * 3 + 1],
+                           model.indices[i * 3 + 2]};
+    
+    // implement tangent calculation here
+  }
+
+  for (unsigned i = 0; i < tangents.size(); ++i) {
+    // implement orthogonalization and normalization here
+  }
+
+  return tangents;
 }
 
 };
